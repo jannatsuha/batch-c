4c791e60dac223ded861f99d486c469025a38acb
jannatsuha242424@gmail.com <24101997Jannat>
2021-10-03 10:34:41 +0600

    First Commit

---
+++ android/app/src/main/kotlin/com/example/batch_c/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.batch_c
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+String btnText="button 1";
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        title:
+        Text("My Application"),
+      ),
+      body: Column(
+        children: [
+          RaisedButton(
+            child: Text(btnText),
+            color: Colors.lightGreen,
+              onPressed: (){
+              setState(() {
+                btnText="Button Pressed";
+              });
+              }
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
